cc7339005c6196b4827b2e05787a2cd091cc1ccd
Sirisha Dudiki <sdudiki@l-ec-1gwdn.local>
2016-01-27 04:05:54 -0800

    Connect LED Home Page Xib

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,16 +10,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        print("digit = \(digit)")
+        
+        if userIsInTheMiddleOfTypingANumber
+        {
+            display.text = display.text! + digit
+        }
+        else
+        {
+            display.text = digit;
+            userIsInTheMiddleOfTypingANumber = true
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+
+        operandStack.append(displayValue)
+        print("Operand Stack = \(operandStack)")
+        
+        userIsInTheMiddleOfTypingANumber = false
     }
-
-
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)";
+        }
+        
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTypingANumber
+        {
+            enter()
+        }
+        
+        switch operation{
+        case "➕":
+            performOperation {$0 + $1}
+        case "➖":
+            performOperation {$1 - $0}
+        case "➗":
+            performOperation {$0 / $1}
+        case "✖️":
+            performOperation {$1 * $0}
+        case "⑀":
+            performOperationOneParameter {sqrt($0)}
+        case "CLR":
+            clear()
+            
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double){
+        if operandStack.count >= 2{
+            displayValue =  operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperationOneParameter(operation: Double -> Double){
+        if operandStack.count >= 1{
+            displayValue =  operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func clear(){
+        userIsInTheMiddleOfTypingANumber = false;
+        operandStack.removeAll();
+        displayValue = 0;
+    }
+    
 }
 
