6e43b33f2664094f4043aec1fa77caa94eac59ee
Sirisha Dudiki <sdudiki@l-ec-1gwdn.local>
2016-01-27 04:35:02 -0800

    changing comments

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         print("digit = \(digit)")
         
-        // Test Git Commit
+        // second comment
         if userIsInTheMiddleOfTypingANumber
         {
             display.text = display.text! + digit
